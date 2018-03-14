2d5b6b3ef2d3a7b030fc72b55c4dd8860508a636
l-ribeiro <luizaugustovr@gmail.com>
2018-03-14 17:56:49 -0300

    add project
    add project

---
+++ routes/index.js
@@ -0,0 +1,47 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+/* GET Userlist page. */
+router.get('/userlist', function(req, res) {
+   var db = require("../db"); //carrega o arquivo db.js
+   var Users = db.Mongoose.model('usercollection', db.UserSchema, 'usercollection');
+   
+   //O lean() é opcional aqui, mas uma boa prática de performance, para retornar um JSON text-plain ao invés de objetos Mongoose complexos
+   Users.find({}).lean().exec(
+      function (e, docs) {
+         res.render('userlist', { "userlist": docs });
+   });
+});
+
+/* GET New User page. */
+router.get('/newuser', function(req, res) {
+	res.render('newuser', { title: 'Add New User' });
+});
+
+/* POST to Add User Service */
+router.post('/adduser', function (req, res) {
+
+    var db = require("../db");
+    var userName = req.body.username;
+    var userEmail = req.body.useremail;
+
+    var Users = db.Mongoose.model('usercollection', db.UserSchema, 'usercollection');
+    var user = new Users({ username: userName, email: userEmail });
+    user.save(function (err) {
+        if (err) {
+            console.log("Error! " + err.message);
+            return err;
+        }
+        else {
+            console.log("Post saved");
+            res.redirect("userlist");
+        }
+    });
+});
+
+module.exports = router;
